b0ba03b44aa76dc5922a01b5d8957d137cde91f6
Rakesh Gupta <rg688407@gmail.com>
2026-09-07 18:55:53 +0530

    updated

--- backend/server.js
+++ backend/server.js
@@ -13,17 +13,17 @@ import supportRoutes from './routes/supportRoutes.js';
 const app = express();
 
 // Middleware
-// app.use(cors({ origin: process.env.CORS_ORIGIN || '*' }));
-// // app.use(cors({
-// //   origin: process.env.CORS_ORIGIN || "*",
-// //   credentials: true
-// // }));
+app.use(cors({ origin: process.env.CORS_ORIGIN || '*' }));
+// app.use(cors({
+//   origin: process.env.CORS_ORIGIN || "*",
+//   credentials: true
+// }));
 
 
-app.use(cors({
-  origin: ['http://localhost:5173', 'https://pfm-with-ai-feature-bidv.vercel.app/'],
-  credentials: true
-}));
+// app.use(cors({
+//   origin: ['http://localhost:5173', 'https://pfm-with-ai-feature-bidv.vercel.app'],
+//   credentials: true
+// }));
 
 
 
--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -7,7 +7,7 @@ import axios from 'axios';
 // });
 
 const api = axios.create({
- baseURL: import.meta.env.VITE_API_URL
+ baseURL:"https://pfm-with-ai-feature-2.onrender.com"
 });
 
 // const api = axios.create({
